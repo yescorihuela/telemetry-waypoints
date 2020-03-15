48f7ebea7996a8fa91a17c4093f355da7cfbc118
Yrvin Escorihuela <yher136@gmail.com>
2020-03-15 17:14:32 -0300

    Add folder of services

---
+++ lib/services/last_waypoints_vehicle.rb
@@ -0,0 +1,22 @@
+module Services
+  class LastWaypointsVehicle
+    def self.get_or_create_vehicle_in_cache(vehicle_identifier, vehicle_obj)
+      if vehicle_identifier.nil?
+        Rails.cache.read(vehicle_identifier)
+      else
+        Rails.cache.write(vehicle_identifier, vehicle_obj)
+      end
+    end
+
+    def self.get_vehicles_latest_waypoints()
+      # TO-DO refactor for others caching providers
+      begin
+        current_cached_data = Rails.cache.redis.keys.select{|s| s =~ /#{Rails.cache.options[:namespace]}/ }
+        current_vehicles = current_cached_data.map{|v| v.split(':')[1]}
+        latest_waypoints = Rails.cache.read_multi(*current_vehicles)
+      rescue
+        []
+      end
+    end
+  end
+end
