49f10b286444b439db02550db62d99e10908b967
Yrvin Escorihuela <yher136@gmail.com>
2020-03-15 15:30:51 -0300

    Refactor for caching the latest coordinate

--- app/workers/gps_measurements_worker.rb
+++ app/workers/gps_measurements_worker.rb
@@ -3,13 +3,14 @@ class GpsMeasurementsWorker
   sidekiq_options queue: :gps_measurements, retry: false, backtrace: false
 
   def perform(waypoint)
-    latitude = waypoint['latitude']
-    longitude = waypoint['longitude']
-    sent_at = waypoint['sent_at']
-    vehicle_identifier = waypoint['vehicle_identifier']
-
-    vehicle = OpenStruct.new(:id => nil)
-    vehicle.id = get_or_create_vehicle_in_cache(vehicle_identifier)
+    vehicle = OpenStruct.new(:id => nil, **waypoint.transform_keys(&:to_sym))
+    vehicle_identifier = vehicle.vehicle_identifier
+    
+    begin
+      vehicle.id = get_or_create_vehicle_in_cache(vehicle)[:id]
+    rescue
+      vehicle.id = nil
+    end
 
     if vehicle.id.nil?
       vehicle = Vehicle.find_by(vehicle_identifier: vehicle_identifier)
@@ -17,32 +18,35 @@ class GpsMeasurementsWorker
         vehicle = Vehicle.new(vehicle_identifier: vehicle_identifier)
         if vehicle.valid?
           vehicle.save!
-          get_or_create_vehicle_in_cache(vehicle_identifier, vehicle.id)
         end
       end
-      get_or_create_vehicle_in_cache(vehicle_identifier, vehicle.id)
+      vehicle = OpenStruct.new(:id => vehicle.id, **waypoint.transform_keys(&:to_sym))
     end
-    create_waypoint(vehicle.id, waypoint)
+    get_or_create_vehicle_in_cache(vehicle)
+    create_waypoint(vehicle, waypoint)
   end
 
 
-  def create_waypoint(vehicle_id, waypoint)
+  def create_waypoint(vehicle, waypoint)
     new_waypoint = OpenStruct.new(waypoint)
-    new_waypoint[:vehicle_id] = vehicle_id
     new_waypoint.delete_field('vehicle_identifier')
+    new_waypoint[:vehicle_id] = vehicle.id 
+
     begin
+
       Waypoint.create!(new_waypoint.to_h)
     rescue => exception
-      # TO-DO
-      # Introduce logging and control exception here
+      Rails.logger.warning exception
     end
   end
 
-  def get_or_create_vehicle_in_cache(vehicle_license_plate, vehicle_id = nil)
-    if vehicle_id.nil?
-      Rails.cache.read(vehicle_license_plate)
+  def get_or_create_vehicle_in_cache(vehicle)
+    vehicle_identifier = vehicle.vehicle_identifier
+
+    if vehicle.id.nil?
+      Rails.cache.read(vehicle_identifier)
     else
-      Rails.cache.write(vehicle_license_plate, vehicle_id)
+      Rails.cache.write(vehicle_identifier, vehicle)
     end
   end
 end
